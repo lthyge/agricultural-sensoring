143804bf00bcf66433d93867b2d20df578c65320
NikolajRFA <nrfadk@gmail.com>
2024-09-23 14:05:47 +0200

    First ESP-NOW test

--- slave/src/main.cpp
+++ slave/src/main.cpp
@@ -1,16 +1,109 @@
-#include <Arduino.h>
+/*
+  Rui Santos & Sara Santos - Random Nerd Tutorials
+  Complete project details at https://RandomNerdTutorials.com/esp-now-two-way-communication-esp32/
+  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files.
+  The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+*/
+#include <esp_now.h>
+#include <WiFi.h>
+
+// REPLACE WITH THE MAC Address of your receiver
+uint8_t broadcastAddress[] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
+
+// Incoming readings
+String incomingMsg;
+
+// Variable to store if sending data was successful
+String success;
+
+// Structure example to send data
+// Must match the receiver structure
+typedef struct struct_message
+{
+    String msg;
+} struct_message;
+
+// Create a struct_message called BME280Readings to hold sensor readings
+struct_message message;
+
+// Create a struct_message to hold incoming sensor readings
+struct_message incomingMessage;
+
+esp_now_peer_info_t peerInfo;
+
+// Callback when data is sent
+void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status)
+{
+    Serial.print("\r\nLast Packet Send Status:\t");
+    Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
+    if (status == 0)
+    {
+        success = "Delivery Success :)";
+    }
+    else
+    {
+        success = "Delivery Fail :(";
+    }
+}
+
+// Callback when data is received
+void OnDataRecv(const uint8_t *mac, const uint8_t *incomingData, int len)
+{
+    memcpy(&incomingMessage, incomingData, sizeof(incomingMessage));
+    Serial.print("Bytes received: ");
+    Serial.println(len);
+    incomingMsg = incomingMessage.msg;
+}
 
 void setup()
 {
-    pinMode(LED_BUILTIN, OUTPUT);
+    // Init Serial Monitor
+    Serial.begin(115200);
 
+    // Set device as a Wi-Fi Station
+    WiFi.mode(WIFI_STA);
+
+    // Init ESP-NOW
+    if (esp_now_init() != ESP_OK)
+    {
+        Serial.println("Error initializing ESP-NOW");
+        return;
+    }
+
+    // Once ESPNow is successfully Init, we will register for Send CB to
+    // get the status of Trasnmitted packet
+    esp_now_register_send_cb(OnDataSent);
+
+    // Register peer
+    memcpy(peerInfo.peer_addr, broadcastAddress, 6);
+    peerInfo.channel = 0;
+    peerInfo.encrypt = false;
+
+    // Add peer
+    if (esp_now_add_peer(&peerInfo) != ESP_OK)
+    {
+        Serial.println("Failed to add peer");
+        return;
+    }
+    // Register for a callback function that will be called when data is received
+    esp_now_register_recv_cb(esp_now_recv_cb_t(OnDataRecv));
 }
 
 void loop()
 {
-    digitalWrite(LED_BUILTIN, HIGH);
-    delay(1000);
-    
-    digitalWrite(LED_BUILTIN, LOW);
-    delay(1000);
+    // Set values to send
+    message.msg = "Hello, World!";
+
+    // Send message via ESP-NOW
+    esp_err_t result = esp_now_send(broadcastAddress, (uint8_t *)&message, sizeof(message));
+
+    if (result == ESP_OK)
+    {
+        Serial.println("Sent with success");
+    }
+    else
+    {
+        Serial.println("Error sending the data");
+    }
+    delay(10000);
 }
